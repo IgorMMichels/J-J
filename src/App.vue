2b6df6994fb41c2c2dc8cca5a3854ea4d6f8a9d3
IgorMMichels <igor.marcon.michels@gmail.com>
2026-02-26 16:03:33 -0300

    Started the main page of the website

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup></script>
+
+<template>
+  <header>
+    <nav>
+      <div class="logo">
+        <h1>J&J</h1>
+        <p>june 23rd, 2025</p>
+      </div>
+
+      <div>
+        <ul>
+          <li>HOME</li>
+          <li>OUR STORY</li>
+          <li>DETAILS</li>
+          <li>RSPV</li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <main>
+    <section class="banner">
+      <h2>JENNY & JASON</h2>
+      <div>
+        <h3>With love and gratitude, we invite you to share in the joy of our wedding day. <br>Saturday, September 20, 2025 The Golden Elm Manor,  St.Augustine, New York</h3>
+      </div>
+    </section>
+    <section class="cards">
+      <div>
+        <div>
+          <h4>OUR STORY</h4>
+        </div>
+        <div>
+          <h4>THE DETAILS</h4>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h4>RSVP</h4>
+        </div>
+        <div>
+          <h4>REGISTRY</h4>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped></style>
